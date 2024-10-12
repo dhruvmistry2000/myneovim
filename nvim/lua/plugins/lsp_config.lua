e413b39934fcefa31852845fb084def583709710
Dhruv Mistry <dhruvmistry2000@gmail.com>
2024-10-12 12:09:41 +0530

    Update lsp_config.lua

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -19,10 +19,6 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
 			lspconfig.pyright.setup({
 				capabilities = capabilities,
 			})
